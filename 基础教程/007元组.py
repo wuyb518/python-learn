20c05e3650eccf76d2dd890022cca7d7189b8052
wuyb518 <wuyb518@outlook.com>
2018-03-02 23:15:38 +0800

    元组

---
+++ 基础教程/007元组.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# _*_ coding:UTF-8 _*_
+
+tup1=('physics','chemistry',1997,2000)
+tup1=(1,2,3,4,5)
+tup3=('a','b','c','d')
+tup4=()
+tup5=(3,) #特别注意,元组中只有一个元素时要加一个逗号,否则就成了表达式了了
+
+# 元组一旦创建就不能被修改了,相当于只读列表
+# 任意无符号的对象,以逗号隔开,默认为uuanzu
+
+print 'abc',-4.32,16+8,'xyz'
+x,y=1,2
+print x,y
+print tuple([1,2,3])
+print tuple({'a':1,'b':2})
+
+
+
+
